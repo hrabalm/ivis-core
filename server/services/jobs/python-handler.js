def2949246398d4a2cb4190235308061d85c89ba
Miroslav Hrabal <mhn@email.cz>
2021-04-29 10:12:00 +0200

    Specify elasticsearch-py version
    Official documentation of the library claims that "The library is
    compatible with all Elasticsearch versions since 0.90.x *but you
    have to use a matching major version*:"
    
    Currently, latest stable version (7.12.1) is installed instead.
    On the other hand, I have not encountered any problem with version
    mismatch so-far.

--- server/services/jobs/python-handler.js
+++ server/services/jobs/python-handler.js
@@ -15,7 +15,7 @@ const IVIS_PCKG_DIR = path.join(__dirname, '..', '..', 'lib', 'tasks', 'python',
 
 const runningProc = new Map();
 
-const defaultPythonLibs = ['elasticsearch', 'elasticsearch_dsl'];
+const defaultPythonLibs = ['"elasticsearch>=6.0.0,<7.0.0"', '"elasticsearch_dsl>=6.0.0,<7.0.0"'];
 const taskSubtypeSpecs = {
     [defaultSubtypeKey]: {
         libs: defaultPythonLibs
